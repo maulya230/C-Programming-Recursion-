1296eec71ab78ee705d5bf09bac24bdf4aaf8662
maulya230 <75597705+maulya230@users.noreply.github.com>
2021-08-23 11:17:58 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int calculatePower(int, int);
+
+int main()
+{
+    int base, power, result;
+
+    cout << "Enter base number: ";
+    cin >> base;
+
+    cout << "Enter power number(positive integer): ";
+    cin >> power;
+
+    result = calculatePower(base, power);
+    cout << base << "^" << power << " = " << result;
+
+    return 0;
+}
+
+int calculatePower(int base, int power)
+{
+    if (power != 0)
+        return (base*calculatePower(base, power-1));
+    else
+        return 1;
+}
